a63f668c397f9dfaada6406922e20bff0496c335
OliverBurge <ollieontheweb@gmail.com>
2019-03-11 14:53:33 +0000

    Implemented search functionality with newsapi call for SearchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import newsApi from "../apis/newsApi";
 
 class App extends React.Component {
 	state = {};
+	onInputSubmit = async input => {
+		const response = await newsApi.get("/everything", {
+			params: {
+				q: input
+			}
+		});
+		console.log(response);
+	};
 	render() {
 		return (
 			<div className="ui container">
-				<SearchBar />
+				<SearchBar onFormSubmit={this.onInputSubmit} />
 			</div>
 		);
 	}
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,29 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-	state = {};
+	state = { input: "" };
+	onInputChange = event => {
+		this.setState({ input: event.target.value });
+		console.log(event.target.value);
+	};
+
+	onFormSubmit = event => {
+		event.preventDefault();
+		this.props.onFormSubmit(this.state.input);
+		console.log(this.state.input);
+	};
+
 	render() {
 		return (
 			<div className="ui segment">
-				<form type="text" className="ui form">
+				<form onSubmit={this.onFormSubmit} className="ui form">
 					<div className="field">
 						<label>Search:</label>
-						<input className="" />
+						<input
+							value={this.state.input}
+							onChange={this.onInputChange}
+							type="text"
+						/>
 					</div>
 				</form>
 			</div>
